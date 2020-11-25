b8bd197119270d8071a8da471c1efd1a38fd90b7
Quang Van <quang@absentsoft.com>
2020-11-25 06:05:56 -0500

    ugh

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import tw, { GlobalStyles } from 'twin.macro';
+import { Button, Logo } from './components';
 
 interface AppProps {}
 
-function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
-}
+// function App({}: AppProps) {
+//   // Create the count state.
+//   const [count, setCount] = useState(0);
+//   // Create the counter (+1 every second).
+//   useEffect(() => {
+//     const timer = setTimeout(() => setCount(count + 1), 1000);
+//     return () => clearTimeout(timer);
+//   }, [count, setCount]);
+//   // Return the App component.
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <p>
+//           Page has been open for <code>{count}</code> seconds.
+//         </p>
+//         <p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App = ({}: AppProps) => (
+  <div>
+    <GlobalStyles />
+    {/* <GlobalStyles />
+    <div
+      css={[
+        tw`flex flex-col items-center justify-center h-screen`,
+        tw`bg-gradient-to-b`,
+      ]}
+    >
+      <div tw="flex flex-col justify-center h-full space-y-5">
+        <Button isPrimary>Submit</Button>
+        <Button isSecondary>Cancel</Button>
+        <Button isSmall>Close</Button>
+      </div>
+      <Logo />
+    </div> */}
+  </div>
+);
 
 export default App;
