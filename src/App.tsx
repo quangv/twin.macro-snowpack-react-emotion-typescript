f4a3f6ea8201158c67648a915e35ec643603dc7c
Quang Van <quang@absentsoft.com>
2020-11-25 16:37:15 -0500

    working

--- src/App.tsx
+++ src/App.tsx
@@ -3,48 +3,13 @@ import { Button, Logo } from './components';
 
 interface AppProps {}
 
-// function App({}: AppProps) {
-//   // Create the count state.
-//   const [count, setCount] = useState(0);
-//   // Create the counter (+1 every second).
-//   useEffect(() => {
-//     const timer = setTimeout(() => setCount(count + 1), 1000);
-//     return () => clearTimeout(timer);
-//   }, [count, setCount]);
-//   // Return the App component.
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <p>
-//           Page has been open for <code>{count}</code> seconds.
-//         </p>
-//         <p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Learn React
-//           </a>
-//         </p>
-//       </header>
-//     </div>
-//   );
-// }
-
 const App = ({}: AppProps) => (
   <div>
     <GlobalStyles />
-    {/* <GlobalStyles />
     <div
       css={[
         tw`flex flex-col items-center justify-center h-screen`,
-        tw`bg-gradient-to-b`,
+        tw`bg-gradient-to-b from-electric to-ribbon`,
       ]}
     >
       <div tw="flex flex-col justify-center h-full space-y-5">
@@ -53,7 +18,7 @@ const App = ({}: AppProps) => (
         <Button isSmall>Close</Button>
       </div>
       <Logo />
-    </div> */}
+    </div>
   </div>
 );
 
